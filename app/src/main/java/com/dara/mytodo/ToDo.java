63004eb6addcb8724d9b3ed785ea4027992a44b7
Oluwadara Abijo <oluwadara.abijo@gmail.com>
2019-12-10 10:34:42 +0100

    Create ToDo item entity

---
+++ app/src/main/java/com/dara/mytodo/ToDo.java
@@ -0,0 +1,127 @@
+package com.dara.mytodo;
+
+import android.os.Parcel;
+import android.os.Parcelable;
+
+import androidx.annotation.NonNull;
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
+
+import java.util.Objects;
+
+/**
+ * This class describes the Entity (which represents the SQLite table) for the To-Do items.
+ * Each public property in the class represents a column in the table.
+ */
+
+@Entity(tableName = "to_do_items_table")
+public class ToDo implements Parcelable {
+    @NonNull
+    @PrimaryKey
+    private String title;
+    private String details;
+    private String category;
+    private String date;
+    private String time;
+    private Boolean isCompleted;
+
+    // Constructor
+    public ToDo(@NonNull String title, String details, String category, String date,
+                String time, Boolean isCompleted) {
+        this.title = title;
+        this.details = details;
+        this.category = category;
+        this.date = date;
+        this.time = time;
+        this.isCompleted = isCompleted;
+    }
+
+    // Parcelable implementation
+    private ToDo(Parcel in) {
+        title = Objects.requireNonNull(in.readString());
+        details = in.readString();
+        category = in.readString();
+        date = in.readString();
+        time = in.readString();
+        byte tmpIsCompleted = in.readByte();
+        isCompleted = tmpIsCompleted == 0 ? null : tmpIsCompleted == 1;
+    }
+
+    public static final Creator<ToDo> CREATOR = new Creator<ToDo>() {
+        @Override
+        public ToDo createFromParcel(Parcel in) {
+            return new ToDo(in);
+        }
+
+        @Override
+        public ToDo[] newArray(int size) {
+            return new ToDo[size];
+        }
+    };
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeString(title);
+        dest.writeString(details);
+        dest.writeString(category);
+        dest.writeString(date);
+        dest.writeString(time);
+        dest.writeByte((byte) (isCompleted == null ? 0 : isCompleted ? 1 : 2));
+    }
+
+    // Getters and setters
+    @NonNull
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(@NonNull String title) {
+        this.title = title;
+    }
+
+    public String getDetails() {
+        return details;
+    }
+
+    public void setDetails(String details) {
+        this.details = details;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getTime() {
+        return time;
+    }
+
+    public void setTime(String time) {
+        this.time = time;
+    }
+
+    public Boolean getCompleted() {
+        return isCompleted;
+    }
+
+    public void setCompleted(Boolean completed) {
+        isCompleted = completed;
+    }
+
+}
