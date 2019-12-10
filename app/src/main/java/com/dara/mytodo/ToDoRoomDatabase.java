872232c81b77ddfc82df2480ea4a2d6e63141c18
Oluwadara Abijo <oluwadara.abijo@gmail.com>
2019-12-10 18:35:20 +0100

    Add a Room database

---
+++ app/src/main/java/com/dara/mytodo/ToDoRoomDatabase.java
@@ -0,0 +1,34 @@
+package com.dara.mytodo;
+
+import android.content.Context;
+
+import androidx.room.Database;
+import androidx.room.Room;
+import androidx.room.RoomDatabase;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+@Database(entities = {ToDoItem.class, ToDoList.class}, version = 1, exportSchema = false)
+public abstract class ToDoRoomDatabase extends RoomDatabase {
+
+    // Get an instance of ToDoDao
+    public abstract ToDoDao toDoDao();
+
+    private static volatile ToDoRoomDatabase INSTANCE;
+    private static final int NUMBER_OF_THREADS = 4;
+    static final ExecutorService databaseWriteExecutor = Executors.
+            newFixedThreadPool(NUMBER_OF_THREADS);
+
+    static ToDoRoomDatabase getDatabase(final Context context) {
+        if (INSTANCE == null) {
+            synchronized (ToDoRoomDatabase.class) {
+                if (INSTANCE == null) {
+                    INSTANCE = Room.databaseBuilder(context.getApplicationContext(),
+                            ToDoRoomDatabase.class, "todo_database").build();
+                }
+            }
+        }
+        return INSTANCE;
+    }
+}
