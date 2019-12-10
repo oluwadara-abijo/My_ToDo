ca8ea381dd7955800597e053bbd1fa2763d813ef
Oluwadara Abijo <oluwadara.abijo@gmail.com>
2019-12-10 11:10:17 +0100

    Define database operations

--- app/src/main/java/com/dara/mytodo/ToDoDao.java
+++ app/src/main/java/com/dara/mytodo/ToDoDao.java
@@ -1,4 +1,41 @@
 package com.dara.mytodo;
 
-public class ToDoDao {
+import androidx.room.Dao;
+import androidx.room.Delete;
+import androidx.room.Insert;
+import androidx.room.Query;
+import androidx.room.Update;
+
+import java.util.List;
+
+/**
+ * This interface contains all the methods used for accessing the database
+ */
+
+@Dao
+public interface ToDoDao {
+
+    @Insert()
+    void insertList(ToDoList toDoList);
+
+    @Insert()
+    void insertItem(ToDoItem toDoItem);
+
+    @Update()
+    void updateList(ToDoList toDoList);
+
+    @Update()
+    void updateItem(ToDoItem toDoItem);
+
+    @Delete()
+    void deleteList(ToDoList toDoList);
+
+    @Delete()
+    void deleteItem(ToDoItem toDoItem);
+
+    @Query("SELECT * FROM list_table")
+    List<ToDoList> getLists();
+
+    @Query("SELECT * FROM to_do_items_table WHERE title==:listTitle ")
+    List<ToDoItem> getToDoItems(String listTitle);
 }
